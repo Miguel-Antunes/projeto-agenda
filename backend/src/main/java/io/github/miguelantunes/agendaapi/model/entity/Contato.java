6ad79e8d2f12f6cceb39048535246f1e01eca444
Miguel-antunes <miguel.antunes.costa10@gmail.com>
2022-09-13 16:33:27 -0300

    upload de arquivo no spring boot

--- backend/src/main/java/io/github/miguelantunes/agendaapi/model/entity/Contato.java
+++ backend/src/main/java/io/github/miguelantunes/agendaapi/model/entity/Contato.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -31,6 +32,7 @@ public class Contato {
 	@Column	
 	private Boolean favorito;
 	
-	
-
+	@Column
+	@Lob
+	private byte[] foto;
 }
